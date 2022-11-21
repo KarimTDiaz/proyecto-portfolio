3c9383cfb82917376fb017d6ca08ddf912a659a1
karimtd <karimtilouni94@gmail.com>
2022-11-21 23:41:29 +0100

    Proyecto portfolio

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,11 @@
+const menu=document.getElementById('menu')
 const menuIcon = document.getElementById('menu-icon')
 const menuIconBar = document.getElementById('menu-icon-bar')
 
 menuIcon.addEventListener('click', () => {
-    menuIconBar.classList.toggle('menu-icon__bar--show')
+    menuIconBar.classList.toggle('menu-icon__bar--show');
+})
+
+menuIcon.addEventListener('click', () => {
+    menu.classList.toggle('menu--show')
 })
